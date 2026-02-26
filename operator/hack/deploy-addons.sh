40b81523618cca22e4035f339e6f00f0ab4d44bc
Ron Kahn <rkahn@nvidia.com>
2026-02-26 11:23:02 +0200

    chore: restore deploy-addons.sh from upstream main
    Signed-off-by: Ron Kahn <rkahn@nvidia.com>

--- operator/hack/deploy-addons.sh
+++ operator/hack/deploy-addons.sh
@@ -22,18 +22,18 @@ set -o pipefail
 
 # This script deploys add-ons to the KIND cluster. Run this script after `make kind-up` has created a KIND cluster.
 # Ensure that the KUBECONFIG points to the KIND cluster and invoke this script.
-#
-# For KWOK and Pyroscope, use create-e2e-cluster.py instead:
-#   python3 hack/e2e-cluster/create-e2e-cluster.py --kwok-nodes N --pyroscope
 
 USAGE=""
 DEPLOY_METRICS_SERVER=false
+DEPLOY_KWOK=false
 
 function create_usage() {
   usage=$(printf '%s\n' "
   usage: $(basename $0) [Options]
   Options:
+    --all | -a                 Deploys all the supported add-ons.
     --metrics-server | -m      Deploys a metrics server if this option is specified.
+    --kwok | -k                Deploys KWOK if this option is specified.
   ")
   echo "${usage}"
 }
@@ -41,8 +41,15 @@ function create_usage() {
 function parse_flags() {
   while test $# -gt 0; do
     case "$1" in
+      --all | -a)
+        DEPLOY_KWOK=true
+        DEPLOY_METRICS_SERVER=true
+        ;;
       --metrics-server | -m)
         DEPLOY_METRICS_SERVER=true
+        ;;
+      --kwok | -k)
+        DEPLOY_KWOK=true
         ;;
       -h | --help)
         shift
@@ -64,6 +71,10 @@ function check_prereq() {
     echo >&2 "kubectl is not installed, please install kubectl from https://kubernetes.io/docs/tasks/tools/install-kubectl/"
     exit 1
   fi
+  if ! command -v jq &> /dev/null; then
+    echo >&2 "jq is not installed, please install jq from https://jqlang.org/download"
+    exit 1
+  fi
 }
 
 # deploy_metrics_server deploys the metrics server.
@@ -78,10 +89,29 @@ function deploy_metrics_server() {
   fi
 }
 
+# deploy_kwok deploys KWOK using the instructions at https://kwok.sigs.k8s.io/docs/user/kwok-in-cluster/
+function deploy_kwok() {
+  if [ "${DEPLOY_KWOK}" = true ]; then
+    local kwok_repo="kubernetes-sigs/kwok"
+    local kwok_latest_release=$(curl "https://api.github.com/repos/${kwok_repo}/releases/latest" | jq -r '.tag_name')
+    printf "Deploying [KWOK]\n"
+    printf "%s\n" "---------------------------------------------------"
+    # deploy KWOK CRDs and controller
+    printf "[KWOK]: Deploying CRDs and controller...\n"
+    kubectl apply -f "https://github.com/${kwok_repo}/releases/download/${kwok_latest_release}/kwok.yaml"
+
+    # setup default custom resources of stages
+    printf "[KWOK]: Setting up default custom resources of stages...\n"
+    kubectl apply -f "https://github.com/${kwok_repo}/releases/download/${kwok_latest_release}/stage-fast.yaml"
+    printf "\n[KWOK]: Deployed successfully!\n"
+  fi
+}
+
 function main() {
   check_prereq
   parse_flags "$@"
   deploy_metrics_server
+  deploy_kwok
 }
 
 USAGE=$(create_usage)
