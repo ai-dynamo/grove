0de4f79383c075db6d7428e339a9d7921a42d3a3
xianlubird <xianlubird@gmail.com>
2026-04-08 21:52:57 +0800

    Revert "operator: add volcano subgroup mapping"
    This reverts commit 7b25f1e24bb30f9bfef2c18b59477184f339defe.

--- operator/internal/controller/podclique/components/pod/pod.go
+++ operator/internal/controller/podclique/components/pod/pod.go
@@ -30,7 +30,6 @@ import (
 	"github.com/ai-dynamo/grove/operator/internal/expect"
 	"github.com/ai-dynamo/grove/operator/internal/resourceclaim"
 	schedmanager "github.com/ai-dynamo/grove/operator/internal/scheduler/manager"
-	volcanoscheduler "github.com/ai-dynamo/grove/operator/internal/scheduler/volcano"
 	"github.com/ai-dynamo/grove/operator/internal/utils"
 	k8sutils "github.com/ai-dynamo/grove/operator/internal/utils/kubernetes"
 
@@ -154,10 +153,6 @@ func (r _resource) buildResource(pcs *grovecorev1alpha1.PodCliqueSet, pclq *grov
 		Labels:       labels,
 		Annotations:  pclq.Annotations,
 	}
-	if pod.ObjectMeta.Annotations == nil {
-		pod.ObjectMeta.Annotations = map[string]string{}
-	}
-	pod.ObjectMeta.Annotations[volcanoscheduler.SubGroupAnnotationKey] = pclq.Name
 	if err = controllerutil.SetControllerReference(pclq, pod, r.scheme); err != nil {
 		return groveerr.WrapError(err,
 			errCodeSetControllerReference,
