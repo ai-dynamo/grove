262a3fbb4b6e5a079bba73e5e5604742ea547a2d
xianlubird <xianlubird@gmail.com>
2026-04-08 18:31:12 +0800

    operator: fix staticcheck for volcano subgroup annotation

--- operator/internal/controller/podclique/components/pod/pod.go
+++ operator/internal/controller/podclique/components/pod/pod.go
@@ -154,10 +154,10 @@ func (r _resource) buildResource(pcs *grovecorev1alpha1.PodCliqueSet, pclq *grov
 		Labels:       labels,
 		Annotations:  pclq.Annotations,
 	}
-	if pod.ObjectMeta.Annotations == nil {
-		pod.ObjectMeta.Annotations = map[string]string{}
+	if pod.Annotations == nil {
+		pod.Annotations = map[string]string{}
 	}
-	pod.ObjectMeta.Annotations[volcanoscheduler.SubGroupAnnotationKey] = pclq.Name
+	pod.Annotations[volcanoscheduler.SubGroupAnnotationKey] = pclq.Name
 	if err = controllerutil.SetControllerReference(pclq, pod, r.scheme); err != nil {
 		return groveerr.WrapError(err,
 			errCodeSetControllerReference,
