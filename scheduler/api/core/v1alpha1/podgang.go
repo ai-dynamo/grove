4a2037060f8ece314044706c915b3692a0a9883c
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2026-06-22 13:14:43 +0200

    fixup! api/crds: add printer columns

--- scheduler/api/core/v1alpha1/podgang.go
+++ scheduler/api/core/v1alpha1/podgang.go
@@ -31,7 +31,6 @@ import (
 // +kubebuilder:printcolumn:name="Ready",type=string,JSONPath=`.status.conditions[?(@.type=="Ready")].status`
 // +kubebuilder:printcolumn:name="Unhealthy",type=string,JSONPath=`.status.conditions[?(@.type=="Unhealthy")].status`,priority=1
 // +kubebuilder:printcolumn:name="Disrupting",type=string,JSONPath=`.status.conditions[?(@.type=="DisruptionTarget")].status`,priority=1
-// +kubebuilder:printcolumn:name="Score",type=number,JSONPath=`.status.placementScore`,priority=1
 // +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
 
 // PodGang defines a specification of a group of pods that should be scheduled together.
