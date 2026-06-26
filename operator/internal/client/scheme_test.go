9fb1e69333703d7b43cdbae894b0fa3988ca3d15
xianlubird <xianlubird@gmail.com>
2026-06-26 09:33:24 +0800

    test: avoid scheduler-specific scheme deps
    Signed-off-by: xianlubird <xianlubird@gmail.com>

--- operator/internal/client/scheme_test.go
+++ operator/internal/client/scheme_test.go
@@ -22,12 +22,9 @@ import (
 	grovecorev1alpha1 "github.com/ai-dynamo/grove/operator/api/core/v1alpha1"
 
 	schedv1alpha1 "github.com/ai-dynamo/grove/scheduler/api/core/v1alpha1"
-	kaitopologyv1alpha1 "github.com/kai-scheduler/KAI-scheduler/pkg/apis/kai/v1alpha1"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
-	volcanov1beta1 "volcano.sh/apis/pkg/apis/scheduling/v1beta1"
 )
 
 func TestSchemeIncludesSharedAPIs(t *testing.T) {
@@ -39,19 +36,4 @@ func TestSchemeIncludesSharedAPIs(t *testing.T) {
 
 	_, err = apiutil.GVKForObject(&schedv1alpha1.PodGang{}, Scheme)
 	require.NoError(t, err)
-}
-
-func TestSchemeDoesNotIncludeSchedulerSpecificAPIs(t *testing.T) {
-	for _, obj := range []struct {
-		name   string
-		object runtime.Object
-	}{
-		{name: "KAI Topology", object: &kaitopologyv1alpha1.Topology{}},
-		{name: "Volcano PodGroup", object: &volcanov1beta1.PodGroup{}},
-	} {
-		t.Run(obj.name, func(t *testing.T) {
-			_, err := apiutil.GVKForObject(obj.object, Scheme)
-			require.Error(t, err)
-		})
-	}
 }
